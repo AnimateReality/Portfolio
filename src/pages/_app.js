318ecdbeb79bc467a9ff49f74ecbf047daf8f3c2
AnimateReality <ronaldchittinappilly@gmail.com>
2022-12-12 18:53:13 +1300

    Update Dark Mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,36 +1,37 @@
 import React, {useState } from "react";
 import Head from "next/head";
 import { ThemeProvider } from 'styled-components';
-
 import { darkTheme, lightTheme } from "../themes/default";
 import GlobalStyles from "../styles/globals";
+
+// this was here
 
 export default function App({ Component, pageProps }) {
 
   const [theme, setTheme ] = useState("light");
+  const isDarkTheme = theme === "dark";
   const themeToggler = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
   const Mode = (props) => (
-    <button onClick={() => themeToggler()}>Dark Mode</button>
+    <button onClick={() => themeToggler()}>{isDarkTheme ?
+            <span aria-label="Light mode" role="img">Light 🌞</span> :
+            <span aria-label="Dark mode" role="img">Dark 🌜</span>}</button>
   );
 
-  const Theme = ({ children }) => (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <Mode />
-      {children}
-    </ThemeProvider>
-  );
-  
+
   return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
     <>
+    
       <Head>
         <title>Ronald Chittinappilly</title>
       </Head>
-      <Theme>
+      
+      <GlobalStyles />
+      <Mode />
         <Component {...pageProps} />
-      </Theme>
     </>
+    </ThemeProvider>
   );
 }
