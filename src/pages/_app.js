a6ab1a4a6947049efe5bb58298d1fc3e291e78c9
AnimateReality <ronaldchittinappilly@gmail.com>
2022-12-12 18:06:22 +1300

    Dark Mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,28 @@
-import Theme from "../styles/theme";
+import React, {useState } from "react";
 import Head from "next/head";
+import { ThemeProvider } from 'styled-components';
+
+import { darkTheme, lightTheme } from "../themes/default";
+import GlobalStyles from "../styles/globals";
 
 export default function App({ Component, pageProps }) {
+
+  const [theme, setTheme ] = useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+  const Mode = (props) => (
+    <button onClick={() => themeToggler()}>Dark Mode</button>
+  );
+
+  const Theme = ({ children }) => (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Mode />
+      {children}
+    </ThemeProvider>
+  );
+  
   return (
     <>
       <Head>
